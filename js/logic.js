e6e94b0f8063ab884658348658d5f733f498e45c
Mohamed Mostafa <mohamedmostafa3730@gmail.com>
2024-11-24 01:28:48 +0200

    update 1:6 add deleteSite() function

--- js/logic.js
+++ js/logic.js
@@ -1,5 +1,6 @@
 var nameOfSite = document.getElementById("siteName");
 var urlOfSite = document.getElementById("siteLink");
+var sites = document.getElementById("sites");
 var websites = [];
 if (localStorage.getItem("sites info") != null) {
   productsList = JSON.parse(localStorage.getItem("sites info"));
@@ -23,23 +24,30 @@ function clear() {
 function display(arr) {
   var printList = ``;
   for (var i = 0; i < arr.length; i++) {
-    printList += `<tr>
-                <th>${i + 1}</th>
-                <th>${arr[i].nameByUser}</th>
-                <th>
-                  <button class="btn btn-outline-info fw-bold">
-                    <i class="fa-solid fa-eye"></i>
-                    Visit
-                  </button>
-                </th>
-                <th>
-                  <button class="btn btn-outline-danger fw-bold">
-                    <i class="fa-solid fa-trash-can"></i>
-                    Delete
-                  </button>
-                </th>
-              </tr>
-   `;
+    printList += `
+    <tr>
+        <th>${i + 1}</th>
+        <th>${arr[i].nameByUser}</th>
+        <th>
+            <button class="btn btn-outline-info fw-bold">
+                <i class="fa-solid fa-eye"></i>
+                Visit
+            </button>
+        </th>
+    <th>
+        <button class="btn btn-outline-danger fw-bold" onclick="deleteSite(${i})"
+            <i class="fa-solid fa-trash-can"></i>
+            Delete
+        </button>
+    </th>
+    </tr>
+    `;
     sites.innerHTML = printList;
   }
+}
+function deleteSite(indexToDelete) {
+  websites.splice(indexToDelete, 1);
+  localStorage.setItem("sites info", JSON.stringify(websites));
+  display(websites);
+  console.log("indexToDelete = " + indexToDelete);
 }
