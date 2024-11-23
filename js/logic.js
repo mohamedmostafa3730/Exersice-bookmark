7e327d44502c2762ce557600a80b69bfa38e4075
Mohamed Mostafa <mohamedmostafa3730@gmail.com>
2024-11-23 04:17:08 +0200

    update 1:5 add display() function and fix some relode issues

--- js/logic.js
+++ js/logic.js
@@ -1,6 +1,10 @@
 var nameOfSite = document.getElementById("siteName");
 var urlOfSite = document.getElementById("siteLink");
 var websites = [];
+if (localStorage.getItem("sites info") != null) {
+  productsList = JSON.parse(localStorage.getItem("sites info"));
+  display(productsList);
+}
 function setSite() {
   var site = {
     nameByUser: nameOfSite.value,
@@ -15,4 +19,27 @@ function setSite() {
 function clear() {
   nameOfSite.value = "";
   urlOfSite.value = "";
+}
+function display(arr) {
+  var printList = ``;
+  for (var i = 0; i < arr.length; i++) {
+    printList += `<tr>
+                <th>${i + 1}</th>
+                <th>${arr[i].nameByUser}</th>
+                <th>
+                  <button class="btn btn-outline-info fw-bold">
+                    <i class="fa-solid fa-eye"></i>
+                    Visit
+                  </button>
+                </th>
+                <th>
+                  <button class="btn btn-outline-danger fw-bold">
+                    <i class="fa-solid fa-trash-can"></i>
+                    Delete
+                  </button>
+                </th>
+              </tr>
+   `;
+    sites.innerHTML = printList;
+  }
 }
