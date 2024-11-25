fd1bb7e74777e85d3393c1c7add58409c15dae4c
Mohamed Mostafa <mohamedmostafa3730@gmail.com>
2024-11-25 02:00:58 +0200

    update 1:8 add update() functions [1] setValuesToUpdate(indexToUpdate) [2] updateItem() [3] and fix some issues

--- js/logic.js
+++ js/logic.js
@@ -2,6 +2,9 @@ var nameOfSite = document.getElementById("siteName");
 var urlOfSite = document.getElementById("siteLink");
 var sites = document.getElementById("sites");
 var searchOf = document.getElementById("searchOf");
+var btnAdd = document.getElementById("Submit");
+var btnUpd = document.getElementById("update");
+var temp;
 var websites = [];
 if (localStorage.getItem("sites info") != null) {
   websites = JSON.parse(localStorage.getItem("sites info"));
@@ -25,6 +28,7 @@ function clear() {
 function display(arr) {
   var printList = ``;
   for (var i = 0; i < arr.length; i++) {
+    // index = i;
     printList += `
     <tr>
         <th>${i + 1}</th>
@@ -34,18 +38,30 @@ function display(arr) {
               target="_blank"
               href="${arr[i].url}"
             >
-              <button class="btn btn-outline-info fw-bold">
+              <button class="btn btn-sm btn-outline-info">
                 <i class="fa-solid fa-eye"></i>
                 Visit
               </button>
             </a>
         </th>
     <th>
-        <button class="btn btn-outline-danger fw-bold" onclick="deleteSite(${i})"
-            <i class="fa-solid fa-trash-can"></i>
-            Delete
+        <button
+          class="btn btn-sm btn-outline-secondary"
+          onclick="setValuesToUpdate(${i})"
+        >
+          <i class="fa-solid fa-pen-to-square"></i>
+          Update
         </button>
-    </th>
+      </th>
+      <th>
+        <button
+          class="btn btn-sm btn-outline-danger"
+          onclick="deleteSite(${i})"
+        >
+          <i class="fa-solid fa-trash"></i>
+          Delete
+        </button>
+      </th>
     </tr>
     `;
     rowData.innerHTML = printList;
@@ -55,7 +71,6 @@ function deleteSite(indexToDelete) {
   websites.splice(indexToDelete, 1);
   localStorage.setItem("sites info", JSON.stringify(websites));
   display(websites);
-  // console.log("indexToDelete = " + indexToDelete);
 }
 
 function search() {
@@ -72,3 +87,19 @@ function search() {
   display(searchArr);
 }
 
+function setValuesToUpdate(indexToUpdate) {
+  nameOfSite.value = websites[indexToUpdate].nameByUser;
+  urlOfSite.value = websites[indexToUpdate].url;
+  btnUpd.classList.remove("d-none");
+  btnAdd.classList.add("d-none");
+  temp = indexToUpdate;
+}
+function updateItem() {
+  websites[temp].nameByUser = nameOfSite.value;
+  websites[temp].url = urlOfSite.value;
+  localStorage.setItem("sites info", JSON.stringify(websites));
+  display(websites);
+  clear();
+  btnUpd.classList.add("d-none");
+  btnAdd.classList.remove("d-none");
+}
